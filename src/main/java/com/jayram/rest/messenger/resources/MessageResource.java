eaf1b2f78682662d4928374c4193b0c0db6abb76
JayramNanoskar <jayram.n978@gmail.com>
2021-07-03 09:46:58 +0530

    Sending status codes and location headers with response

--- src/main/java/com/jayram/rest/messenger/resources/MessageResource.java
+++ src/main/java/com/jayram/rest/messenger/resources/MessageResource.java
@@ -1,5 +1,7 @@
 package com.jayram.rest.messenger.resources;
 
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.List;
 
 import javax.ws.rs.BeanParam;
@@ -12,7 +14,11 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
+import javax.ws.rs.core.Response.Status;
 
 import com.jayram.rest.messenger.model.Message;
 import com.jayram.rest.messenger.resources.beans.MessageFilterBean;
@@ -38,8 +44,14 @@ public class MessageResource {
 	@POST
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Produces(MediaType.APPLICATION_JSON)
-	public Message addMessage(Message message){
-		return messageService.addMessage(message);
+	public Response addMessage(Message message, @Context UriInfo uriInfo){
+		Message newMessage = messageService.addMessage(message);
+		URI uri = uriInfo.getAbsolutePathBuilder()
+						.path(String.valueOf(newMessage.getId()))
+						.build(); //Using Builder pattern to build URI
+		return Response.created(uri) //Setting Location value in Response header & sending 201-Created status code
+				.entity(newMessage)
+				.build(); //Using Builder pattern to build Response
 	}
 	
 	@PUT
